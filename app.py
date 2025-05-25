1958fc0ba166e1db22357388af38f2da13fc3b14
Ayush Patil <patil.sayush@gmail.com>
2025-05-26 04:35:44 +0530

    Added Indain Timezone

--- app.py
+++ app.py
@@ -1,7 +1,36 @@
+import pytz
 from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+import pytz
 from main import GetPrice, SendEmail
+from apscheduler.schedulers.background import BackgroundScheduler
+
+
+def indian_time():
+    tz = pytz.timezone('Asia/Kolkata')
+    return datetime.now(tz)
+
+
+def check_prices():
+    with app.app_context():
+        print("Running Scheduler price checks...")
+        price_checker = GetPrice()
+        email_sender = SendEmail()
+        products = Amazon.query.filter_by(notified=False).all()
+        for product in products:
+            try:
+                current_price = price_checker.get_price(product.url)
+                current_price = float(current_price.replace(",", ""))
+
+                if current_price <= product.target:
+                    email_sender.send_email(product.email)
+                    product.notified = True
+                    db.session.commit()
+                    print(f"Email Sent to {product.email} for {product.title}")
+            except Exception as e:
+                print(f"Error checking price for {product.title}: {e}")
+
 
 app = Flask(__name__)
 
@@ -17,7 +46,8 @@ class Amazon(db.Model):
     url = db.Column(db.String(5000), nullable=False)
     title = db.Column(db.String(500))
     target = db.Column(db.Integer, nullable=False)
-    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+    date_created = db.Column(db.DateTime, default=indian_time)
+    notified = db.Column(db.Boolean, default=False)
 
     def __repr__(self) -> str:
         return f"{self.email} - {self.title}"
@@ -30,12 +60,13 @@ def create_tables():
 @app.route("/home", methods=['GET', 'POST'])
 def home():
     title = None
+    message = None
     if request.method == 'POST':
         email = request.form['email']
         url = request.form['url']
         target = request.form['target']
 
-        if not url.startswith("http: //") and not url.startswith("https://"):
+        if not url.startswith("http://") and not url.startswith("https://"):
             url = "https://" + url
 
         price_checker = GetPrice()
@@ -47,21 +78,22 @@ def home():
 
         print(f"Received email: {email}, url: {url}, title: {title}, Target_Price = {target}")
         existing = Amazon.query.get(email)
-
+        message = None
         if target:
             try:
                 target = int(target)
                 if float(price.replace(",", "")) <= target:
-                    message = f"🎯 Target Price Hit! Price = ₹{price}, Title: {title}"
+                    message = f"📈 Price ₹{price} is still above your target ₹{target}"
+
                     send_email = SendEmail()
-                    email = send_email.send_email(email)
+                    send_email.send_email(email)
+
                 else:
                     message = f"📈 Price ₹{price} is still above your target ₹{target}"
 
             except Exception as target_price_exception:
                 message = f"⚠️ Invalid price format or conversion issue: {target_price_exception}"
 
-            redirect("/home")
         if not existing:
             new_entry = Amazon(email=email, url=url, title=title, target=target)
             db.session.add(new_entry)
@@ -76,7 +108,13 @@ def home():
 # def login():
 #     pass
 
+scheduler = BackgroundScheduler()
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(debug=True, port=5000)
+
+        scheduler = BackgroundScheduler()
+        scheduler.add_job(check_prices, 'interval', hours=1)
+        scheduler.start()
+
+    app.run(debug=True, port=8000)
